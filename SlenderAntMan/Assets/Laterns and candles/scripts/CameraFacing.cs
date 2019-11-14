eb16123d78d8741af895d6bf9f324b152d1a2857
Blomios <blomios@gmail.com>
2019-11-14 02:15:49 +0100

    add clouds, VRWater, candles + pentagramme, wind, fog, set skybox...

---
+++ SlenderAntMan/Assets/Laterns and candles/scripts/CameraFacing.cs
@@ -0,0 +1,19 @@
+﻿//	CameraFacing.cs 
+//	original by Neil Carter (NCarter)
+//	modified by Hayden Scott-Baron (Dock) - http://starfruitgames.com
+//  allows specified orientation axis
+
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraFacing : MonoBehaviour
+{
+	public Camera cameraToLookAt;
+	void Awake() {
+		cameraToLookAt = Camera.main; }
+
+    private void FixedUpdate()
+    {
+    }
+}
